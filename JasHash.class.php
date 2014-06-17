0462753a5643172d6f06527c57d7cbb49fa81a5b
JawadSoft <boudouar6@gmail.com>
2014-06-17 14:26:48 +0100

    change the className

--- JasHash.class.php
+++ JasHash.class.php
@@ -1,46 +1,39 @@
 <?php
 
-class Encrypter {
+class Jas_Hash {
 
 
 	public function __construct(){
-	
+			//
 	}
 	
 	public function get_next ($array,$key){
 		$array_keys = array_keys($array);
-
-		foreach($array_keys as $keyx => $val){
+		foreach($array_keys as $keyx => $val)
 			if($val == $key) return $array_keys[$keyx+1];
-		}
-		//return $array_keys[$key];
 	}
 
 	
 	public function key_data_builder($array = array()){
 		if(!count($array)) return false;
 		$ret = "";
-		foreach($array as $key => $val){
+		foreach($array as $key => $val)
 			$ret .= $key.':'.$val.':';
-		}
 		return $ret;
 	}
 	
 	
 	public	 function key_data_extractor($text = ""){
 		$explode = explode(':',$text);
-		for($i=0;$i<count($explode);$i=$i+2){
+		for($i=0;$i<count($explode);$i=$i+2)
 			$array[$explode[$i]] = $explode[$i+1];
-			
-		}
 		return $array;
 	}
 	
 	
-	public function encrypte($text=""){
+	public function encrypt($text=""){
 		
 		$text = array_map('ord',str_split($text));
-		//print_r(($text));
 		arsort($text);		
 		$first_key = null;
 		$i=0;
@@ -49,24 +42,21 @@ class Encrypter {
 			if(($first_key && $text[$first_key]-$val >= 10) || !$first_key){
 				$first_key = $key;
 				$keys[$i] = $val;
-				//$vals[$i] = $val;
 			}
 			$array[$key] = $text[$first_key]-$val;
 			$i++;	
 		}
-		//ksort($array);
 		
 		return ($this->key_data_builder($keys).implode($array).'-'.(implode("-",array_keys($text))));
 
 	}
-	public function decrypte($text=""){
+	public function decrypt($text=""){
 		$pos = strrpos(($text), ':');
 		if($pos === false) return false;
 		$pos2 = strpos($text,'-');
 		$array = str_split(substr($text,$pos+1,$pos2-$pos-1));
 		$array_of_keys_sorting = explode('-',substr($text,$pos2+1));
 		
-		//	arsort($array);	
 		$keys = $this->key_data_extractor(substr($text,0,$pos));
 		$l=1;
 		foreach($keys as $key => $val){
@@ -92,18 +82,10 @@ class Encrypter {
 		}
 		
 		
-		foreach($array_of_keys_sorting as $kw => $vv){
+		foreach($array_of_keys_sorting as $kw => $vv)
 			$new_array[$vv] = $array[$kw];
-		}
-		
 		ksort($new_array);
 		return implode(array_map('chr',$new_array));
 	}
 	
 }
-echo '<pre>';
-$nn = new Encrypter();
-//echo $nn->encrypte("xxxxx"); 
-print_r($nn->decrypte("0:117:5:105:14:73:15:32:01368001777888000-11-13-2-10-6-15-5-4-1-16-9-14-8-3-0-12-7")); //
-
-?>
